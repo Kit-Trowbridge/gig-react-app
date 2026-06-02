a9fe62e3cf526e523f8e4a8d127b6a11c90a9673
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-02 18:25:00 +0100

    gig tests passing

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -3,8 +3,8 @@ const Gig = (props) => {
         <div className="gig">   
             <h3 className="name">{props.name}</h3>
             <img className="image" src={props.image} alt={`promo image for ${props.name}`} />
-            <p className="description">{props.description}</p>
-            <p className="timeAndDate">{props.timeAndDate.toLocaleString([], {
+            <p className="description" data-testid="description">{props.description}</p>
+            <p className="timeAndDate" data-testid="timeAndDate">{props.timeAndDate.toLocaleString([], {
                 weekday: 'long',
                 day: 'numeric',
                 month: 'numeric',
@@ -12,25 +12,10 @@ const Gig = (props) => {
                 hour: 'numeric',
                 minute: '2-digit',
             })}</p>
-            <p className="location">{props.location}</p>
+            <p className="location" data-testid="location">{props.location}</p>
         </div>
 
     )
 }
 
 export default Gig
-
-
-// Create a component called Gig, which contains information about a gig on the website. It should contain:
-
-// A h3 (heading) element listing the name of the band
-// A img (image) element for an image of the band, using the src attribute to link to an image.
-// A p (paragraph) element containing a description of the event
-// A p element containing the time and date of the event
-// A p element containing the location of the event
-// You can come up with the event details yourself.
-
-// Render this component as a child of the App component.
-
-// Each element should have a descriptive className attached.
-// The component should import a Gig.css file. Add some styling based on the class names you gave your elements to make it look nice.
--- tests/Gig.test.jsx
+++ tests/Gig.test.jsx
@@ -1,7 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Gig from "../src/components/Gig";
 
-test("it renders the correct artist name", () => {
-    render(<Gig name="Test Name"/>);
+test("it renders the correct gig details", () => {
+    render(<Gig 
+        name="Test Name"
+        image="https://t3.ftcdn.net/jpg/05/99/83/02/360_F_599830201_hqWRq7fWPcyuHsiEZb4h1uGOsAS7wSDf.jpg"
+        description="Test Genre"
+        timeAndDate={new Date("January 1, 2028 12:00:00")}
+        location="Springfield, USA"
+    />);
     expect(screen.getByRole("heading")).toHaveTextContent("Test Name");
+    expect(screen.getByRole("img")).toHaveAttribute("src", "https://t3.ftcdn.net/jpg/05/99/83/02/360_F_599830201_hqWRq7fWPcyuHsiEZb4h1uGOsAS7wSDf.jpg");
+    expect(screen.getByTestId("description")).toHaveTextContent("Test Genre");
+    expect(screen.getByTestId("timeAndDate")).toHaveTextContent("01/01/2028") // full text content is Saturday, 01/01/2028, 12:00;
+    expect(screen.getByTestId("location")).toHaveTextContent("Springfield, USA");
 })
