be19de8c7dede5e6f8e56a9d7c46f7e5f8d86e73
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-02 10:08:41 +0100

    Gig file set up

---
+++ src/components/Gig.jsx
@@ -0,0 +1,18 @@
+const Gig = () => {
+
+}
+
+
+// Create a component called Gig, which contains information about a gig on the website. It should contain:
+
+// A h3 (heading) element listing the name of the band
+// A img (image) element for an image of the band, using the src attribute to link to an image.
+// A p (paragraph) element containing a description of the event
+// A p element containing the time and date of the event
+// A p element containing the location of the event
+// You can come up with the event details yourself.
+
+// Render this component as a child of the App component.
+
+// Each element should have a descriptive className attached.
+// The component should import a Gig.css file. Add some styling based on the class names you gave your elements to make it look nice.
