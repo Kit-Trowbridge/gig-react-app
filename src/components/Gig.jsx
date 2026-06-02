ff721d5f03c9c6acbb00da3a739fc0075e147096
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-02 10:29:14 +0100

    hardcoding Gig component data

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,17 +1,31 @@
-const Gig = (props) => {
+const timeAndDate = new Date("July 31, 2026 21:30:00")
+
+const Gig = () => {
     return(
         <div className="gig">   
-            <h3 className="name">{props.name}</h3>
-            <img className="image" src={props.image} alt={`promo image for ${props.name}`} />
-            <p className="description">{props.description}</p>
-            <p className="timeAndDate">{props.timeAndDate.toString()}</p>
-            <p className="location">{props.location}</p>
+            <h3 className="name">Blood Orange</h3>
+            <img className="image" src="https://media.vogue.co.uk/photos/5f7af392a67cbe91f90e282b/2:3/w_2560%2Cc_limit/shutterstock_editorial_9930677ev.jpg" alt="promo image for Blood Orange" />
+            <p className="description">Alternative R&B</p>
+            <p className="timeAndDate">{timeAndDate.toDateString()}</p>
+            <p className="location">London, UK</p>
         </div>
-
     )
 }
 
-export default Gig;
+// const Gig = (props) => {
+//     return(
+//         <div className="gig">   
+//             <h3 className="name">{props.name}</h3>
+//             <img className="image" src={props.image} alt={`promo image for ${props.name}`} />
+//             <p className="description">{props.description}</p>
+//             <p className="timeAndDate">{props.timeAndDate.toString()}</p>
+//             <p className="location">{props.location}</p>
+//         </div>
+
+//     )
+// }
+
+export default Gig
 
 
 // Create a component called Gig, which contains information about a gig on the website. It should contain:
