cd04460d0fb432ed604b907fda3bac57b79399ff
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-03 17:47:51 +0100

    refactor FavouriteButton onClick function

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -3,13 +3,8 @@ import { useState } from "react";
 const FavouriteButton = () => {
     const [favourited, setFavourited] = useState(false);
 
-    const changeHeartBasedOnCurrentState = () => {
-        // if favourited is already true, set it to false and vice versa
-        setFavourited(favourited ? false : true)
-    }
-
     return(
-        <button onClick={changeHeartBasedOnCurrentState}> {favourited ? "❤️" : "♡"} </button>
+        <button onClick={() => setFavourited(!favourited)}> {favourited ? "❤️" : "♡"} </button>
     )
 
 }
