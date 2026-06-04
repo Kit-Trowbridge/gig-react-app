11db2f81d5c696c127b2dad61e2b1c7dd0046d50
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-04 14:19:38 +0100

    create Gigs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Hello from "./components/Hello";
+import Gigs from "./components/Gigs.jsx";
 import Gig from "./components/Gig.jsx";
-import makersLogo from "./assets/Makers-Logo.png";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Gig 
+    <Gigs />
+      {/* <Gig 
         name="Blood Orange"
         image="https://media.vogue.co.uk/photos/5f7af392a67cbe91f90e282b/2:3/w_2560%2Cc_limit/shutterstock_editorial_9930677ev.jpg"
         description="Alternative R&B"
@@ -19,7 +19,7 @@ function App() {
         description="Italo Disco"
         timeAndDate={new Date("February 3, 2027 19:00:00")}
         location="Rome, IT"
-      />
+      /> */}
     </>
   );
 }
